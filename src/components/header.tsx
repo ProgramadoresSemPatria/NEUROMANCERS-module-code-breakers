b2f3929674812ff3c08bf54b1d7293577021aa92
Dnreikronos <dnreikronos2@gmail.com>
2024-12-07 16:11:14 -0300

    style: adjust space between buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,8 @@ import logo from '../assets/símbolo.png';
 const Header: React.FC = () => {
   return (
     <header className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-800 text-white shadow-lg fixed w-full top-0 left-0 z-10">
-      <div className="container mx-auto px-6 py-4 flex items-center justify-between">
+      <div className="container mx-auto px-6 py-4 flex items-center">
+        {/* Left section with logo and brand name */}
         <div className="flex items-center space-x-3">
           <img
             src={logo}
@@ -16,7 +17,8 @@ const Header: React.FC = () => {
           </a>
         </div>
 
-        <nav className="hidden md:flex flex-1 justify-center space-x-6">
+        {/* Center section with navigation links */}
+        <nav className="flex-1 flex justify-center space-x-10">
           <a
             href="/roadmap"
             className="text-lg text-gray-300 hover:text-white transition-transform transform hover:scale-105 font-bold relative group"
@@ -26,7 +28,8 @@ const Header: React.FC = () => {
           </a>
         </nav>
 
-        <div className="hidden md:flex space-x-4">
+        {/* Right section with 'Get Started' button */}
+        <div className="space-x-4">
           <a
             href="/getStarted"
             className="bg-green-500 text-white px-4 py-2 rounded-lg shadow hover:bg-green-600 transition-colors"
