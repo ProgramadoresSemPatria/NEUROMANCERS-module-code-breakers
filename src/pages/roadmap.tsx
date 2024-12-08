c596fd326dff29a3f4cfb5052906a05fd5e5bb57
Dnreikronos <dnreikronos2@gmail.com>
2024-12-08 20:22:14 -0300

    feat: add Progress component in roadmap page

--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactFlowProvider } from "@xyflow/react";
 import Header from "../components/header.tsx";
 import Roadmap from "../components/Roadmap/Roadmap";
-
+import ProgressIndicator from "../components/Progress/ProgressIndicator.tsx"
 const RoadmapPage: React.FC = () => {
   return (
     <>
@@ -10,9 +10,9 @@ const RoadmapPage: React.FC = () => {
       <ReactFlowProvider>
         <Roadmap />
       </ReactFlowProvider>
+			<ProgressIndicator />
     </>
   );
 };
 
 export default RoadmapPage;
-
